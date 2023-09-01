f6b2a70ebcf13dad0fbc6d611c0a40617c97f6f1
Matheus Mauricio <121787652+PedrelliMath@users.noreply.github.com>
2023-09-01 14:04:32 -0300

    Add files via upload

--- queue.cpp
+++ queue.cpp
@@ -18,14 +18,35 @@ struct queue* createQueue(void){
     fila->vector = (int*) malloc(sizeof(int) * maxSize);
     fila->fim = 0;
     fila->maxSize = maxSize;
+
+    if(fila != nullptr){
+        cout << "Fila criada com sucesso!\n" << endl;
+    }else{
+        cout << "Ops, houve um problema ao criar a fila\n" << endl;
+    }
+
     return fila;
 }
 
 void displayMainOptions(void){
-    cout << "1.\tCriar uma fila\n2.\tInserir elemento na fila\n3.\tRemover elemento da fila\n4.\tApagar Fila\n0.\tSair" << endl;
+
+    cout << "1.   Criar uma fila\n"
+     << "2.   Inserir elemento na fila\n"
+     << "3.   Remover elemento da fila\n"
+     << "4.   Apagar Fila\n"
+     << "5.   Imprimir Fila\n"
+     << "0.   Sair" << endl;
 }
 
 void insertItem(struct queue* fila){
+
+    if(fila == nullptr){
+        cout << "Nao existe nenhuma fila...\n" << endl;
+        return;
+    }else if(fila->fim >= fila->maxSize){
+        cout << "A fila esta cheia...\n";
+        return;
+    }
 
     int value = 0;
     cout << "Digite o valor para inserir: " << endl;
@@ -35,6 +56,63 @@ void insertItem(struct queue* fila){
     fila->fim++;
 }
 
+void removeFromQueue(struct queue* fila){
+
+    int index = 0;
+
+    if(fila != nullptr){
+ 
+        cout << "Digite o indice para remover: " << endl;
+        cin >> index;
+        
+        if(index < 0 || index > fila->fim - 1){
+
+            cout << "Indice invalido...\n";
+            return;
+        }
+
+        for(int i = 0; i <= index; i++){
+
+            cout << "Valor a ser removido: " << fila->vector[0] << endl;
+            for(int j = 0; j < fila->fim - 1; j++){
+
+                fila->vector[j] = fila->vector[j + 1];
+            }
+            fila->fim--; 
+        }
+    }else{
+
+        cout << "Nao existe nenhuma fila...\n" << endl;
+    }
+}
+
+void printQueue(struct queue* fila){
+
+    if(fila != nullptr){
+        for(int i = 0; i < fila->fim; i++){
+            
+            cout << fila->vector[i] << " ";
+        }
+        cout << "\n";
+    }else{
+
+        cout << "Nao existe fila para imprimir...\n" << endl;
+    }
+}
+
+void deleteQueue(struct queue** fila) {
+
+    if (*fila != nullptr) {
+
+        free((*fila)->vector);
+        free(*fila);
+        *fila = nullptr;
+        cout << "Fila deletada com sucesso...\n" << endl;
+    } else {
+
+        cout << "Nao existe fila para deletar...\n" << endl;
+    }
+}
 
 int main(void){
 
@@ -42,20 +120,31 @@ int main(void){
     int option = -1;
 
     do{
-
         displayMainOptions();
         cin >> option;
 
         switch(option){
 
             case 0: 
+                deleteQueue(&fila);
                 break;
             case 1: 
                 fila = createQueue();
-                cout << "Fila criada com sucesso!" << endl;
                 break;
             case 2:
                 insertItem(fila);
+                break;
+            case 3:
+                removeFromQueue(fila);
+                break;
+            case 4:
+                deleteQueue(&fila);
+                break;
+            case 5:
+                printQueue(fila);
+                break;
+            default:
+                cout << "Opcao invalida...\n";
                 break;
         }
     }while(option != 0);
